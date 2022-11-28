3694bbb0c0223b5a1d2b4d9630b38808dc64aaab
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-11-28 22:05:05 +0200

    return a random word

--- main.py
+++ main.py
@@ -11,8 +11,21 @@ def choose_category(categoryList):
             category = input("Scrie numarul categoriei dorite ")
 
 
+def rendom_word(category, fileList):
+    try:
+        f = io.open(fileList[category - 1], "r", encoding='utf-8')
+        words = []
+        for line in f:
+            words.append(line.strip())
+        f.close()
+        return words[random.randint(0, len(words))]
+    except:
+        print("Unable to open file ", fileList[category])
+
+
 if __name__ == "__main__":
     fileList = ["animale.txt", "film.txt", "sport.txt"]
     categoryList = ["1. Animale si pasari ", "2. Film", "3. Sport"]
 
     category = choose_category(categoryList)
+    word = rendom_word(category, fileList)
