7f022175d02e2feaf8c6b9d1e8c542850e6c21dd
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-11-28 22:08:25 +0200

    guess the word

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def choose_category(categoryList):
             category = input("Scrie numarul categoriei dorite ")
 
 
-def rendom_word(category, fileList):
+def random_word(category, fileList):
     try:
         f = io.open(fileList[category - 1], "r", encoding='utf-8')
         words = []
@@ -23,9 +23,47 @@ def rendom_word(category, fileList):
         print("Unable to open file ", fileList[category])
 
 
+def print_word(word):
+    cryptotext = []
+    mistakes = int(len(word) / 2)
+    for i in range(0, len(word)):
+        cryptotext.append('_')
+    string = ''
+    for i in cryptotext:
+        string += i
+    print("Cuvantul:")
+    print(string)
+    diacritics = 'ăîâșț'
+    while mistakes > 0 and cryptotext.count('_') > 0:
+        print("Numar de greseli ramase:", mistakes)
+        letter = input("Litera ta este: ").lower()
+        if letter <= 'z' and letter >= 'a' or diacritics.count(letter):
+            ok = False
+            for i in range(0, len(word)):
+                if word[i] == letter and cryptotext[i] != letter:
+                    cryptotext[i] = letter
+                    ok = True
+            if ok:
+                string = ''
+                for i in cryptotext:
+                    string += i
+                print(string)
+            else:
+                mistakes -= 1
+        elif len(letter) > 1:
+            print("Introduceti o singura litera")
+        else:
+            print("Caracter invalid")
+
+    print("Cuvantul este: ", word)
+    print("Numarul de incercari esuate este: ", int(len(word) / 2) - mistakes)
+    write_score(word, int(len(word) / 2) - mistakes)
+
+
 if __name__ == "__main__":
     fileList = ["animale.txt", "film.txt", "sport.txt"]
     categoryList = ["1. Animale si pasari ", "2. Film", "3. Sport"]
 
     category = choose_category(categoryList)
-    word = rendom_word(category, fileList)
+    word = random_word(category, fileList)
+    print_word(word)
